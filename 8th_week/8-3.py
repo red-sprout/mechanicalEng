c6268ec10833dde5d0aeb46b11b3c843ba92d959
Jaewan Joo <80093089+red-sprout@users.noreply.github.com>
2021-08-29 21:38:59 +0900

    8주차 예시

--- 8th_week/8-3.py
+++ 8th_week/8-3.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 n,m = map(int,input().split())
 trees = list(map(int,input().split()))
 left,right = 1,max(trees)
@@ -16,22 +15,3 @@ while left <= right:
     else:
         right = mid-1
 print(answer) 
-=======
-n,m = map(int,input().split())
-trees = list(map(int,input().split()))
-left,right = 1,max(trees)
-answer = 0
-while left <= right:
-    mid = (left+right)//2
-    total = 0
-    for tree in trees:
-        remain = tree-mid
-        if remain > 0:
-            total += remain         
-    if total >= m:
-        left = mid+1
-        answer = max(answer,mid)
-    else:
-        right = mid-1
-print(answer) 
->>>>>>> 288e1950fe9402704d526ecb729f2141d73199c3
