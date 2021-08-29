7831b5430709dfb498e33aa46ff5361f5c07297e
Jaewan Joo <80093089+red-sprout@users.noreply.github.com>
2021-08-29 21:38:34 +0900

    8주차 예시

--- 8th_week/8-2.py
+++ 8th_week/8-2.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 N = int(input())
 result = 0
 s = [0]*(N+1)
@@ -18,24 +17,3 @@ def dfs(cnt):
                 dfs(cnt+1)
 dfs(1)
 print(result)
-=======
-N = int(input())
-result = 0
-s = [0]*(N+1)
-def istrue(x):
-    for i in range(1,x):
-        if s[x]==s[i] or abs(s[x]-s[i])==abs(x-i):
-            return False
-    return True
-def dfs(cnt):
-    global result
-    if cnt>N:
-        result+=1
-    else:
-        for i in range(1,N+1):
-            s[cnt] = i
-            if istrue(cnt):
-                dfs(cnt+1)
-dfs(1)
-print(result)
->>>>>>> 288e1950fe9402704d526ecb729f2141d73199c3
