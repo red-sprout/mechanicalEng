3694bff1235745e8eed2e42b0f336d260bb786c3
Jaewan Joo <80093089+red-sprout@users.noreply.github.com>
2021-08-29 21:38:07 +0900

    8주차 예시

--- 8th_week/8-1.py
+++ 8th_week/8-1.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 N = int(input())
 def bs():
     start = 0
@@ -13,19 +12,3 @@ def bs():
             start = mid+1
     return -1
 print(bs())
-=======
-N = int(input())
-def bs():
-    start = 0
-    end = N
-    while start <= end:
-        mid = (start+end)//2
-        if mid*mid == N:
-            return mid
-        elif mid*mid > N:
-            end = mid-1
-        else:
-            start = mid+1
-    return -1
-print(bs())
->>>>>>> 288e1950fe9402704d526ecb729f2141d73199c3
